a9e3e218506bce4317744833729a262c3f5a0a55
qwer0114 <rnalgh0114@gmail.com>
2026-05-05 14:12:34 +0900

    feat: wds 설치

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "pretendard/dist/web/static/pretendard.css";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "@wanteddev/wds";
+import "@wanteddev/wds/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
