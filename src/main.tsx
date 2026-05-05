3fa0b07c90d57bc1502a030b95e2b295f6562580
qwer0114 <rnalgh0114@gmail.com>
2026-05-05 14:01:03 +0900

    feat: wanted design system 사용

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'pretendard/dist/web/static/pretendard.css'
-import './index.css'
-import App from './App.tsx'
-// import { ThemeProvider } from '@wanteddev/wds'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "pretendard/dist/web/static/pretendard.css";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "@wanteddev/wds";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <ThemeProvider> */}
+    <ThemeProvider>
       <App />
-    {/* </ThemeProvider> */}
+    </ThemeProvider>
   </StrictMode>,
-)
+);
