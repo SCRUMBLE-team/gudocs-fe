5a1317227a7a321fc7e2670e169859bc6661db0b
qwer0114 <rnalgh0114@gmail.com>
2026-05-26 19:53:19 +0900

    feat: base url 수정

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const server_axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 200000,
   withCredentials: true,
 });
